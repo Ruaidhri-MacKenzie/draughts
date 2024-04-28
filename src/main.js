84bc4904f13b1c92bccd18e60687f4113a4978f6
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2024-04-28 17:10:34 +0100

    Returned to top-level await

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import { createState } from "./draughts.js";
-import { resizeCanvas, draw, initImages } from "./render.js";
+import { initImages, resizeCanvas, draw } from "./render.js";
 import { onLeftClick, onRightClick } from "./input.js";
 
 const canvas = document.getElementById("game-window");
 const state = createState();
 
-const main = async () => {
-	await initImages();
-	resizeCanvas(canvas, state);
-	draw(canvas, state);
-	canvas.addEventListener("click", onLeftClick(canvas, state));
-	canvas.addEventListener("contextmenu", onRightClick(canvas, state));
-};
-
-main();
+await initImages();
+resizeCanvas(canvas, state);
+draw(canvas, state);
+canvas.addEventListener("click", onLeftClick(canvas, state));
+canvas.addEventListener("contextmenu", onRightClick(canvas, state));
